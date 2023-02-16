e185ad7dbcdd6e5c46068de8aba6479d3f35fce6
AlekseiSaunders <alekseisaunders@gmail.com>
2023-02-16 16:37:55 -0700

    add GuessList component, guesses state, and addGuess function

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import GuessInput from '../GuessInput/GuessInput';
+import GuessList from '../GuessList/GuessList';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -10,9 +11,22 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
+  const [guesses, setGuesses] = React.useState([]);
+
+  function addGuess(guess) {
+    let newGuess = {
+      guess,
+      id: Math.random(),
+    };
+    let nextGuesses = [...guesses, newGuess];
+    setGuesses(nextGuesses);
+    console.log(nextGuesses);
+  }
+
   return (
     <>
-      <GuessInput />
+      <GuessList guesses={guesses} />
+      <GuessInput addGuess={addGuess} />
     </>
   );
 }
