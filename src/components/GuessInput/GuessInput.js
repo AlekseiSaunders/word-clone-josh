2f844d92b4a11b18212e4881d406484f921f0037
AlekseiSaunders <alekseisaunders@gmail.com>
2023-02-16 16:12:52 -0700

    add GuessInput jsx and state handling of guess

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React from 'react';
 
 function GuessInput() {
-  return <div />;
+  const [guess, setGuess] = React.useState('');
+
+  return (
+    <>
+      <form
+        className="guess-input-wrapper"
+        onSubmit={(event) => {
+          event.preventDefault();
+          console.log(guess);
+          setGuess('');
+        }}
+      >
+        <label htmlFor="guess-input">Enter Guess:</label>
+        <input
+          required
+          id="guess-input"
+          type="text"
+          pattern="[a-zA-Z]{5,5}"
+          value={guess}
+          onChange={(event) => {
+            setGuess(event.target.value.toUpperCase());
+          }}
+        ></input>
+      </form>
+      <p>Your Guess is: {guess}</p>
+    </>
+  );
 }
 
 export default GuessInput;
