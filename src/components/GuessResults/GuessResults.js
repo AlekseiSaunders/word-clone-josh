a144cf112d9481366908d52529c9884da215b311
Aleksei Saunders <alekseisaunders@gmail.com>
2023-02-17 13:23:23 -0700

    move guess square rendering to separate component Guess

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,34 +1,14 @@
 import React from 'react';
+import Guess from '../Guess';
 import { range } from '../../utils';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 function GuessResults({ guesses }) {
   return (
     <div className="guess-results">
-      {range(NUM_OF_GUESSES_ALLOWED).map((num) => {
-        if (!guesses[num]) {
-          return (
-            <p className="guess" key={num}>
-              {range(5).map((num) => {
-                return <span className="cell" key={num}></span>;
-              })}
-            </p>
-          );
-        } else {
-          let guessText = Array.from(guesses[num].guess);
-          return (
-            <p className="guess" key={num}>
-              {guessText.map((letter, index) => {
-                return (
-                  <span className="cell" key={index}>
-                    {letter}
-                  </span>
-                );
-              })}
-            </p>
-          );
-        }
-      })}
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess value={guesses[num] ? guesses[num].guess : null} key={num} />
+      ))}
     </div>
   );
 }
