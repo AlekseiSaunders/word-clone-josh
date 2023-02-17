e074ce0d69231ade53825fda0f49bdce77f4beeb
Aleksei Saunders <alekseisaunders@gmail.com>
2023-02-17 14:23:09 -0700

    past answer as prop through GuessResults

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -3,11 +3,15 @@ import Guess from '../Guess';
 import { range } from '../../utils';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
-function GuessResults({ guesses }) {
+function GuessResults({ guesses, answer }) {
   return (
     <div className="guess-results">
       {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
-        <Guess value={guesses[num] ? guesses[num].guess : null} key={num} />
+        <Guess
+          value={guesses[num] ? guesses[num] : null}
+          answer={answer}
+          key={num}
+        />
       ))}
     </div>
   );
