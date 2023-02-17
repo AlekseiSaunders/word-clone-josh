aeba9013b19b6058fb06bb990f3194f2ee679e6e
Aleksei Saunders <alekseisaunders@gmail.com>
2023-02-17 14:24:06 -0700

    add conditional to render status styling to each letter based on answer

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers';
 
-function Guess({ value }) {
-  return (
-    <p className="guess">
-      {range(5).map((num) => (
-        <span key={num} className="cell">
-          {value ? value[num] : undefined}
-        </span>
-      ))}
-    </p>
-  );
+function Guess({ value, answer }) {
+  if (!value) {
+    return (
+      <p className="guess">
+        {range(5).map((num) => (
+          <span key={num} className="cell"></span>
+        ))}
+      </p>
+    );
+  }
+  if (value) {
+    let styles = checkGuess(value, answer);
+    let letterStatus = styles.map((letter) => letter.status);
+    return (
+      <p className="guess">
+        {range(5).map((num) => (
+          <span key={num} className={`cell ${letterStatus[num]}`}>
+            {value ? value[num] : undefined}
+          </span>
+        ))}
+      </p>
+    );
+  }
 }
 
 export default Guess;
