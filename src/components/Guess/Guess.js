3cb91cf3872b63b223efd918f0517e677bc775f4
Aleksei Saunders <alekseisaunders@gmail.com>
2023-02-17 15:01:49 -0700

    change from conditional to addition nested component for cell rendering

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -2,29 +2,48 @@ import React from 'react';
 import { range } from '../../utils';
 import { checkGuess } from '../../game-helpers';
 
+function Cell({ letter, status }) {
+  const className = !status ? 'cell' : `cell ${status}`;
+
+  return <span className={className}>{letter}</span>;
+}
+
 function Guess({ value, answer }) {
-  if (!value) {
-    return (
-      <p className="guess">
-        {range(5).map((num) => (
-          <span key={num} className="cell"></span>
-        ))}
-      </p>
-    );
-  }
-  if (value) {
-    let styles = checkGuess(value, answer);
-    let letterStatus = styles.map((letter) => letter.status);
-    return (
-      <p className="guess">
-        {range(5).map((num) => (
-          <span key={num} className={`cell ${letterStatus[num]}`}>
-            {value ? value[num] : undefined}
-          </span>
-        ))}
-      </p>
-    );
-  }
+  const result = checkGuess(value, answer);
+
+  return (
+    <p className="guess">
+      {range(5).map((num) => (
+        <Cell
+          key={num}
+          letter={result ? result[num].letter : undefined}
+          status={result ? result[num].status : undefined}
+        />
+      ))}
+    </p>
+  );
+  // if (!value) {
+  //   return (
+  //     <p className="guess">
+  //       {range(5).map((num) => (
+  //         <span key={num} className="cell"></span>
+  //       ))}
+  //     </p>
+  //   );
+  // }
+  // if (value) {
+  //   let styles = checkGuess(value, answer);
+  //   let letterStatus = styles.map((letter) => letter.status);
+  //   return (
+  //     <p className="guess">
+  //       {range(5).map((num) => (
+  //         <span key={num} className={`cell ${letterStatus[num]}`}>
+  //           {value ? value[num] : undefined}
+  //         </span>
+  //       ))}
+  //     </p>
+  //   );
+  // }
 }
 
 export default Guess;
